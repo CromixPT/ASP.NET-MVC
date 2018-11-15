7f2a977433f7263df721b3fa7711fc2c32cfb09c
CromixPT <tiago.g.candeias@gmail.com>
2018-11-15 02:48:45 +0000

    Section 3 - Lesson 29 Query the database for customer info and populate the custumer view with that data.

--- Vidly_V2/Vidly_V2/Models/Customer.cs
+++ Vidly_V2/Vidly_V2/Models/Customer.cs
@@ -1,8 +1,14 @@
-﻿namespace Vidly_V2.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vidly_V2.Models
 {
     public class Customer
     {
         public int Id { get; set; }
+        //Use of DataAnnotations to be able to change EntityFramework default conventions
+        // in this case a string being null and unlimited size.
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
         public MembershipType MembershipType { get; set; }
